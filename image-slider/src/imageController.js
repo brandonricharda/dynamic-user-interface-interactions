8308967e6da1debff9be3618bc2cd99fdc69e04e
brandonricharda <brandonricharda@gmail.com>
2021-07-28 22:56:54 -0400

    Add nav dots

--- image-slider/src/imageController.js
+++ image-slider/src/imageController.js
@@ -1,9 +1,13 @@
 import findHashKeyByValue from "./findHashKeyByValue";
 
 const imageController = (function() {
-    let activeImage = document.getElementById("active-image")
+    let activeImage = document.getElementById("active-image");
     let previousButton = document.getElementById("previous-button");
     let nextButton = document.getElementById("next-button");
+
+    let navDotOne = document.getElementById("nav-dot-1");
+    let navDotTwo = document.getElementById("nav-dot-2");
+    let navDotThree = document.getElementById("nav-dot-3");
 
     let images = {
         1: "./images/golden-gate.jpeg",
@@ -12,6 +16,30 @@ const imageController = (function() {
     }
 
     let imageCount = Object.keys(images).length;
+
+    let clearNavDots = function() {
+        [navDotOne, navDotTwo, navDotThree].forEach(function(navDot) {
+            navDot.classList.remove("active-nav-dot");
+        });
+    }
+
+    navDotOne.addEventListener("click", function() {
+        clearNavDots();
+        activeImage.setAttribute("src", images[1]);
+        navDotOne.classList.add("active-nav-dot");
+    });
+
+    navDotTwo.addEventListener("click", function() {
+        clearNavDots();
+        activeImage.setAttribute("src", images[2]);
+        navDotTwo.classList.add("active-nav-dot");
+    });
+
+    navDotThree.addEventListener("click", function() {
+        clearNavDots();
+        activeImage.setAttribute("src", images[3]);
+        navDotThree.classList.add("active-nav-dot");
+    });
 
     previousButton.addEventListener("click", function() {
         let src = activeImage.getAttribute("src");
@@ -28,9 +56,13 @@ const imageController = (function() {
         let src = activeImage.getAttribute("src");
         let currentKey = findHashKeyByValue(images, src);
         if (currentKey == imageCount) {
+            clearNavDots();
+            navDotOne.classList.add("active-nav-dot");
             activeImage.setAttribute("src", images[1]);
         } else {
             let newKey = parseInt(currentKey) + 1;
+            clearNavDots();
+            [navDotOne, navDotTwo, navDotThree][newKey - 1].classList.add("active-nav-dot");
             activeImage.setAttribute("src", images[newKey]);
         }
     });
--- image-slider/src/index.js
+++ image-slider/src/index.js
@@ -1,5 +1,3 @@
 const { default: imageController } = require("./imageController");
-const { default: navButtonController } = require("./navButtonController");
 
-navButtonController();
 imageController();
